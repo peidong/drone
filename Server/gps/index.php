c426adb6e6fa4e9e00418bcc2de09593ab659d9b
peidong <peidong@outlook.com>
2015-10-02 15:05:53 -0700

    server gps api

--- Server/gps/index.php
+++ Server/gps/index.php
@@ -1,10 +1,11 @@
 <?php
     header("Content-Type:application/json");
     include("process.php");
-    if(!empty($_GET['name'])){
-        $name = $_GET['name'];
-        $name = process($name);
-        deliver_response(200,"Thank you for your request","Welcome to the GPS part, $name");
+    if(!empty($_GET['latitude'])){
+        $latitude = $_GET['latitude'];
+        $latitude = process($latitude);
+        $longitude = $_GET['longitude'];
+        deliver_response(200,"Thank you for your request","Welcome to the GPS part, the latitude is: $latitude, the longitude is: $longitude");
     }else{
         deliver_response(400,"Bad Request",NULL);
     }
