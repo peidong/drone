fa0a193cd4e4be7d293123c696d5c467a1f2c183
peidong <peidong@outlook.com>
2015-10-04 15:44:30 -0700

    mysql update

--- Server/mysql/initialize.sql
+++ Server/mysql/initialize.sql
@@ -6,3 +6,5 @@ CREATE TABLE IF NOT EXISTS edison.led (
     `led_on_ms` int(11) DEFAULT 0,
     `led_off_ms` int(11) DEFAULT 0
     ) ENGINE = InnoDB;
+
+INSERT INTO `led` VALUES ('1', '0', '0', '0');
