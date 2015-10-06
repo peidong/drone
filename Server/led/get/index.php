d7c353b0fd8e6b4cd774a6fcd80ac87ca795e822
peidong <peidong@outlook.com>
2015-10-06 14:13:27 -0700

    led update

---
+++ Server/led/get/index.php
@@ -0,0 +1,33 @@
+<?php
+    header("Content-Type:application/json");
+
+    $conn = mysql_connect('localhost', 'webmaster', '');
+    mysql_select_db('edison', $conn);
+    
+    $query = "SELECT id, led_status, led_on_ms, led_off_ms
+        FROM led
+        WHERE id = '1'";
+
+    $result = mysql_query($query);
+    $result_array = mysql_fetch_array($result);
+
+    $led_status = $result_array[led_status];
+    $led_on_ms = $result_array[led_on_ms];
+    $led_off_ms = $result_array[led_off_ms];
+
+    $response['led_status'] = $led_status;
+    $response['led_on_ms'] = $led_on_ms;
+    $response['led_off_ms'] = $led_off_ms;
+
+    deliver_response(200, "The ip address has been got", $response);
+
+    function deliver_response($status,$status_message,$data){
+        header("HTTP/1.1 $status $status_message");
+        $response['status']=$status;
+        $response['status_message']=$status_message;
+        $response['data']=$data;
+
+        $json_response=json_encode($response);
+        echo $json_response;
+    }
+?>
---
+++ Server/led/post/index.php
@@ -0,0 +1,42 @@
+<?php
+    header("Content-Type:application/json");
+
+    $ip_address = $_POST['ip_address'];
+    $network_name = $_POST['network_name'];
+
+    $conn = mysql_connect('localhost', 'webmaster', '');
+    mysql_select_db('edison', $conn);
+    
+    $query = "UPDATE ip_address
+        SET ip_address = '$ip_address',network_name = '$network_name', update_time = now()
+        WHERE id = '1'";
+
+    $result = mysql_query($query);
+
+    $query = "SELECT id, ip_address, network_name, update_time
+        FROM ip_address
+        WHERE id = '1'";
+
+    $result = mysql_query($query);
+    $result_array = mysql_fetch_array($result);
+
+    $ip_address = $result_array[ip_address];
+    $network_name = $result_array[network_name];
+    $update_time = $result_array[update_time];
+
+    $response['ip_address'] = $ip_address;
+    $response['network_name'] = $network_name;
+    $response['update_time'] = $update_time;
+
+    deliver_response(200, "The ip_address has been updated", $response);
+
+    function deliver_response($status,$status_message,$data){
+        header("HTTP/1.1 $status $status_message");
+        $response['status']=$status;
+        $response['status_message']=$status_message;
+        $response['data']=$data;
+
+        $json_response=json_encode($response);
+        echo $json_response;
+    }
+?>
