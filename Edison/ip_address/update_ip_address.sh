0292a9274f23b7494076124d1876398eba272c61
peidong <peidong@outlook.com>
2015-10-06 20:25:59 +0000

    post updat

--- Edison/ip_address/update_ip_address.sh
+++ Edison/ip_address/update_ip_address.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 ip_address=`ifconfig wlan0 | grep "inet " | awk -F'[: ]+' '{ print $4 }'`
-curl "http://fryer.ee.ucla.edu/rest/api/ip_address/update/?ip_address=$ip_address"
+network_name=`iwgetid -r`
+curl --data "ip_address=$ip_address&network_name=$network_name" "http://fryer.ee.ucla.edu/rest/api/ip_address/post/"
