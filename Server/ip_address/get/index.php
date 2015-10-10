b26a71d923e3d645abd20024e4ec0113aa5866ae
peidong <peidong@outlook.com>
2015-10-10 09:50:55 -0700

    mac address update

--- Server/ip_address/get/index.php
+++ Server/ip_address/get/index.php
@@ -10,11 +10,31 @@
     $conn = mysql_connect('localhost', 'webmaster', '');
     mysql_select_db('edison', $conn);
     
-    for ($i=1; $i<=5; $i++)
-    {
+    //for ($i=1; $i<=1; $i++)
+    //{
+    //$query = "SELECT id, ip_address, mac_address, network_name, update_time
+        //FROM ip_address
+        //WHERE id='$i'";
+
+    //$result = mysql_query($query);
+    //$result_array = mysql_fetch_array($result);
+
+    //$ip_address = $result_array[ip_address];
+    //$mac_address = $result_array[mac_address];
+    //$network_name = $result_array[network_name];
+    //$update_time = $result_array[update_time];
+
+    //$response['ip_address'] = $ip_address;
+    //$response['mac_address'] = $mac_address;
+    //$response['network_name'] = $network_name;
+    //$response['update_time'] = $update_time;
+
+    //$response_array[$i] = $response;
+    //}
+    
     $query = "SELECT id, ip_address, mac_address, network_name, update_time
         FROM ip_address
-        WHERE id='$i'";
+        WHERE id='1'";
 
     $result = mysql_query($query);
     $result_array = mysql_fetch_array($result);
@@ -29,10 +49,7 @@
     $response['network_name'] = $network_name;
     $response['update_time'] = $update_time;
 
-    $response_array[$i] = $response;
-    }
-
-    deliver_response(200, "The ip address has been got", $response_array);
+    deliver_response(200, "The ip address has been got", $response);
 
     function deliver_response($status,$status_message,$data){
         header("HTTP/1.1 $status $status_message");
