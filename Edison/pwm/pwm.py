6ca07fbdc81225691aca2903ff36b61fd7686f17
peidong <peidong@outlook.com>
2015-10-15 14:51:27 -0700

    pwm update

--- Edison/pwm/pwm.py
+++ Edison/pwm/pwm.py
@@ -1,19 +1,41 @@
+#!/usr/bin/env python
 import mraa
 import time
-#x = mraa.Gpio(3)
-x = mraa.Pwm(6)
-x.period_us(20000)
+import requests
+import json
+import yaml
+import threading
 
-x.enable(True)
-value= 0.03
+pwm1 = mraa.Pwm(6)
+pwm2 = mraa.Pwm(7)
+pwm3 = mraa.Pwm(8)
+pwm4 = mraa.Pwm(9)
+
+pwm1.period_us(20000)
+pwm2.period_us(20000)
+pwm3.period_us(20000)
+pwm4.period_us(20000)
+
+pwm1.enable(True)
+pwm2.enable(True)
+pwm3.enable(True)
+pwm4.enable(True)
 
 while True:
 
+    results_raw = requests.get("http://fryer.ee.ucla.edu/rest/api/pwm/get/")
+    results_json = results_raw.text
+    results = yaml.safe_load(results_json)
+    pwm1_value = double(results['data']['pwm1'])
+    pwm2_value = double(results['data']['pwm2'])
+    pwm3_value = double(results['data']['pwm3'])
+    pwm4_value = double(results['data']['pwm4'])
+
     while value<0.1:
-          x.write(value)
-          value = value + 0.001
+          pwm1.write(pwm1_value)
+          pwm1_value = pwm1_value + 0.001
           time.sleep(0.005)
-    while value>0.03:
-          x.write(value)
-          value = value - 0.001
+    while pwm1_value>0.03:
+          pwm1.write(pwm1_value)
+          pwm1_value = value - 0.001
           time.sleep(0.005)
