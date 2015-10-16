33cbeee50ebd86d0fe089d7c54c6c3e0da65e61e
peidong <peidong@outlook.com>
2015-10-16 15:49:08 -0700

    pwm update

--- Edison/pwm/pwm_basic.py
+++ Edison/pwm/pwm_basic.py
@@ -2,13 +2,14 @@ import mraa
 import time
 
 x = mraa.Pwm(3)
+# period_us is 700 us
 x.period_us(700)
 x.enable(True)
 value= 0.0
 
 while True:
     x.write(value)
-    time.sleep(0.05)
+    # time.sleep(0.05)
     value = value + 0.01
     if value >= 1:
         value = 0.0
