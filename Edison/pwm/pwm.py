e863240ead5960b6f5e3671fc155af3f97bcbeaa
peidong <peidong@outlook.com>
2015-10-16 15:24:08 -0700

    pwm update

--- Edison/pwm/pwm.py
+++ Edison/pwm/pwm.py
@@ -7,19 +7,19 @@ import yaml
 import threading
 
 pwm1 = mraa.Pwm(3)
-pwm2 = mraa.Pwm(5)
-pwm3 = mraa.Pwm(6)
-pwm4 = mraa.Pwm(9)
+# pwm2 = mraa.Pwm(5)
+# pwm3 = mraa.Pwm(6)
+# pwm4 = mraa.Pwm(9)
 
 pwm1.period_us(20000)
-pwm2.period_us(20000)
-pwm3.period_us(20000)
-pwm4.period_us(20000)
+# pwm2.period_us(20000)
+# pwm3.period_us(20000)
+# pwm4.period_us(20000)
 
 pwm1.enable(True)
-pwm2.enable(True)
-pwm3.enable(True)
-pwm4.enable(True)
+# pwm2.enable(True)
+# pwm3.enable(True)
+# pwm4.enable(True)
 
 while True:
 
@@ -27,13 +27,13 @@ while True:
     results_json = results_raw.text
     results = yaml.safe_load(results_json)
     pwm1_value = float(results['data']['pwm1'])
-    pwm2_value = float(results['data']['pwm2'])
-    pwm3_value = float(results['data']['pwm3'])
-    pwm4_value = float(results['data']['pwm4'])
+    # pwm2_value = float(results['data']['pwm2'])
+    # pwm3_value = float(results['data']['pwm3'])
+    # pwm4_value = float(results['data']['pwm4'])
     print "pwm1 = " + str(pwm1_value)
-    print "pwm2 = " + str(pwm2_value)
-    print "pwm3 = " + str(pwm3_value)
-    print "pwm4 = " + str(pwm4_value)
+    # print "pwm2 = " + str(pwm2_value)
+    # print "pwm3 = " + str(pwm3_value)
+    # print "pwm4 = " + str(pwm4_value)
 
     while pwm1_value<0.1:
           pwm1.write(pwm1_value)
