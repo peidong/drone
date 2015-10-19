7d0f09bf3c91db876e908c2da94ff62185fe7900
peidong <peidong@outlook.com>
2015-10-19 13:46:13 -0700

    http update

--- Edison/http/main.c
+++ Edison/http/main.c
@@ -7,7 +7,7 @@ int main(void)
     content = http_get("http://fryer.ee.ucla.edu/rest/api/pwm/get/");
     printf("\n%s", content);
     printf("\n\nStart HTTP POST");
-    content = http_post("http://fryer.ee.ucla.edu/rest/api/pwm/post/", "");
+    content = http_post("http://fryer.ee.ucla.edu/rest/api/pwm/post/", "pwm1=1.23&pwm2=2.34&pwm3=3.45&pwm4=4.56");
     printf("\n%s", content);
     return 0;
 }
