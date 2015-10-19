4bb72c8c6534e8ac43c9743630fdf5af0311b7a3
byyangyang <byyangyang@ucla.edu>
2015-10-19 01:53:54 -0700

    send HTTP GET information to char point variable content

--- Edison/http/http.h
+++ Edison/http/http.h
@@ -3,16 +3,32 @@
 //opkg update
 //opkg install libcurl
 #include <curl/curl.h>
+#include <curl/easy.h>
+#include <string.h>
 
 //json-c (https://github.com/json-c/json-c) 
 //opkg update
 //opkg install libjson
 //#include <json-c/json.h>
  
+
+
+
+/* the function to invoke as the data recieved */
+size_t static write_callback_func(void *buffer, size_t size, size_t nmemb, void *userp)
+{
+    char **response_ptr =  (char**)userp;
+
+    /* assuming the response is a string */
+    *response_ptr = strndup(buffer, (size_t)(size *nmemb));
+    return 0;
+}
 char* http_get(char* url)
 {
     CURL *curl;
     CURLcode res;
+    char *response=NULL;
+
 
     /* In windows, this will init the winsock stuff */ 
     curl_global_init(CURL_GLOBAL_ALL);
@@ -21,20 +37,31 @@ char* http_get(char* url)
     curl = curl_easy_init();
     if(curl) {
         curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_HTTPGET, 1);
+
+    /* follow locations specified by the response header */
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1);
+
+    /* setting a callback function to return the data */
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback_func);
+
+    /* passing the pointer to the response as the callback parameter */
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
 
     /* Perform the request, res will get the return code */ 
         res = curl_easy_perform(curl);
 
     /* Check for errors */ 
-        if(res != CURLE_OK)
+        if(res != CURLE_OK){
             fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-
+        }
     /* always cleanup */ 
         curl_easy_cleanup(curl);
         curl_global_cleanup();
     }
-    return 0;
+    return response;
 }
+
 
 char* http_post(char* url, char* post_data)
 {
--- Edison/http/main.c
+++ Edison/http/main.c
@@ -1,8 +1,11 @@
 #include "http.h"
 
 int main(void)
-{
-        http_get("http://fryer.ee.ucla.edu/rest/api/ip_address/get/");
-        http_post("http://fryer.ee.ucla.edu/rest/api/ip_address/post/", "ip_address=192.168.0.1&mac_address=fc:c2:de:37:78:ce&network_name=test");
+{       char* content;
+       // content = http_get("http://fryer.ee.ucla.edu/rest/api/ip_address/get/");
+        content = http_get("http://fryer.ee.ucla.edu/rest/api/pwm/get/");
+        printf("/n%s", content);
+        
+        //http_post("http://fryer.ee.ucla.edu/rest/api/ip_address/post/", "ip_address=192.168.0.1&mac_address=fc:c2:de:37:78:ce&network_name=test");
               return 0;
 }
