afd329a43bd33023a026249d43547e22006ee958
peidong <peidong@outlook.com>
2015-10-18 22:09:41 -0700

    http update

--- Edison/http/http.h
+++ Edison/http/http.h
@@ -1,5 +1,13 @@
 #include <stdio.h>
+//libcurl (http://curl.haxx.se/libcurl/c) 
+//opkg update
+//opkg install libcurl
 #include <curl/curl.h>
+
+//json-c (https://github.com/json-c/json-c) 
+//opkg update
+//opkg install libjson
+#include <json-c/json.h>
  
 char* http_get(char* url)
 {
@@ -12,15 +20,11 @@ char* http_get(char* url)
     /* get a curl handle */ 
     curl = curl_easy_init();
     if(curl) {
-    /* First set the URL that is about to receive our POST. This URL can
-       just as well be a https:// URL if that is what should receive the
-       data. */ 
         curl_easy_setopt(curl, CURLOPT_URL, url);
-    /* Now specify the POST data */ 
-        /*curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "ip_address=192.168.0.1&mac_address=fc:c2:de:37:78:ce&network_name=test");*/
 
     /* Perform the request, res will get the return code */ 
         res = curl_easy_perform(curl);
+
     /* Check for errors */ 
         if(res != CURLE_OK)
             fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
--- Edison/http/main.c
+++ Edison/http/main.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-        http_get("http://fryer.ee.ucla.edu/rest/api/ip_address/get/index.php");
-            http_post("http://fryer.ee.ucla.edu/rest/api/ip_address/post/index.php", "ip_address=192.168.0.1&mac_address=fc:c2:de:37:78:ce&network_name=test");
+        http_get("http://fryer.ee.ucla.edu/rest/api/ip_address/get/");
+        http_post("http://fryer.ee.ucla.edu/rest/api/ip_address/post/", "ip_address=192.168.0.1&mac_address=fc:c2:de:37:78:ce&network_name=test");
               return 0;
 }
