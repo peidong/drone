4905c9623c72f90a85c72d61d7652ead44c3963b
peidong <peidong@outlook.com>
2015-10-22 17:49:34 -0700

    pwm update

--- Edison/pwm/pwm.c
+++ Edison/pwm/pwm.c
@@ -1,12 +1,12 @@
-/*#include <mraa/pwm.h>*/
+#include <mraa/pwm.h>
 #include "../include/drone_include.h"
 int main()
 {
-    /*mraa_pwm_context pwm;*/
-    /*pwm = mraa_pwm_init(3);*/
-    /*if (pwm == NULL) {*/
-        /*return 1;*/
-    /*}*/
+    mraa_pwm_context pwm;
+    pwm = mraa_pwm_init(3);
+    if (pwm == NULL) {
+        return 1;
+    }
     struct T_pwm *pT_pwm_all;
     double arrd_pwm[4];
     char *arrsz_pwm_key_name[4] = {"pwm1", "pwm2", "pwm3", "pwm4"};
@@ -17,17 +17,17 @@ int main()
         printf("%s's value is %f\n", arrsz_pwm_key_name[n_index], arrd_pwm[n_index]);
     }
 
-    /*mraa_pwm_period_us(pwm, 200);*/
-    /*mraa_pwm_enable(pwm, 1);*/
-    /*float value = 0.0f;*/
-    /*while (1) {*/
-        /*value = value + 0.01f;*/
-        /*mraa_pwm_write(pwm, value);*/
-        /*usleep(50000);*/
-        /*if (value >= 1.0f) {*/
-            /*value = 0.0f;*/
-        /*}*/
-        /*float output = mraa_pwm_read(pwm);*/
-    /*}*/
+    mraa_pwm_period_us(pwm, 200);
+    mraa_pwm_enable(pwm, 1);
+    float value = 0.0f;
+    while (1) {
+        value = value + 0.01f;
+        mraa_pwm_write(pwm, value);
+        usleep(50000);
+        if (value >= 1.0f) {
+            value = 0.0f;
+        }
+        float output = mraa_pwm_read(pwm);
+    }
     return 0;
 }
