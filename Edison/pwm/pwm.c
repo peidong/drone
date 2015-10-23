9d9f5a1315d325724a7e81bf8f7cfe3ef6e815a7
peidong <peidong@outlook.com>
2015-10-22 17:58:55 -0700

    pwm update

--- Edison/pwm/pwm.c
+++ Edison/pwm/pwm.c
@@ -7,26 +7,22 @@ int main()
     if (pwm == NULL) {
         return 1;
     }
+    mraa_pwm_period_us(pwm, 200);
+    mraa_pwm_enable(pwm, 1);
     struct T_pwm *pT_pwm_all;
     double arrd_pwm[4];
     char *arrsz_pwm_key_name[4] = {"pwm1", "pwm2", "pwm3", "pwm4"};
     int n_index;
-    pT_pwm_all = GetPwmStruct();
-    for(n_index = 0; n_index < 4; n_index++){
-        arrd_pwm[n_index] = GetPwmValue(pT_pwm_all, arrsz_pwm_key_name[n_index]);
-        printf("%s's value is %f\n", arrsz_pwm_key_name[n_index], arrd_pwm[n_index]);
-    }
-
-    mraa_pwm_period_us(pwm, 200);
-    mraa_pwm_enable(pwm, 1);
-    float value = 0.0f;
-    while (1) {
-        value = value + 0.01f;
-        mraa_pwm_write(pwm, value);
-        usleep(50000);
-        if (value >= 1.0f) {
-            value = 0.0f;
+    while(1)
+    {
+        pT_pwm_all = GetPwmStruct();
+        for(n_index = 0; n_index < 4; n_index++){
+            arrd_pwm[n_index] = GetPwmValue(pT_pwm_all, arrsz_pwm_key_name[n_index]);
+            printf("%s's value is %f\n", arrsz_pwm_key_name[n_index], arrd_pwm[n_index]);
         }
+
+        mraa_pwm_write(pwm, arrd_pwm[0]);
+        usleep(50000);
         float output = mraa_pwm_read(pwm);
     }
     return 0;
