218ea31865508d88d3e75ee33f81b34e360e49e3
peidong <peidong@outlook.com>
2015-10-25 21:44:46 -0700

    edit server control

--- Server/control/get/index.php
+++ Server/control/get/index.php
@@ -4,26 +4,37 @@
     // start the session
     session_start();
 
+    $mac_address = $_GET['mac_address'];
+
     // close the session
     session_write_close();
 
     $conn = mysql_connect('localhost', 'webmaster', '');
     mysql_select_db('edison', $conn);
     
-    $query = "SELECT id, mac_address, ip_address, network_name, control_type, auto_control_command, manual_control_command, update_time
+    $query = "SELECT *
         FROM control
-        WHERE id = '1'";
+        WHERE mac_address = $mac_address";
 
     $result = mysql_query($query);
     $result_array = mysql_fetch_array($result);
 
-    $ip_address = $result_array[ip_address];
-    $ip_address = $result_array[ip_address];
-    $network_name = $result_array[network_name];
+    $control_type = $result_array[control_type];
+    $auto_control_command = $result_array[auto_control_command];
+    $manual_control_command = $result_array[manual_control_command];
+    $suspend_pwm1 = $result_array[suspend_pwm1];
+    $suspend_pwm2 = $result_array[suspend_pwm2];
+    $suspend_pwm3 = $result_array[suspend_pwm3];
+    $suspend_pwm4 = $result_array[suspend_pwm4];
     $update_time = $result_array[update_time];
 
-    $response['ip_address'] = $ip_address;
-    $response['network_name'] = $network_name;
+    $response['control_type'] = $control_type;
+    $response['auto_control_command'] = $auto_control_command;
+    $response['manual_control_command'] = $manual_control_command;
+    $response['suspend_pwm1'] = $suspend_pwm1;
+    $response['suspend_pwm2'] = $suspend_pwm2;
+    $response['suspend_pwm3'] = $suspend_pwm3;
+    $response['suspend_pwm4'] = $suspend_pwm4;
     $response['update_time'] = $update_time;
 
     deliver_response(200, "The ip address has been got", $response);
