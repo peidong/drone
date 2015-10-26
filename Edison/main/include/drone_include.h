21b4f2e32888a870b2714f0b38fcc962fb275e2b
peidong <peidong@outlook.com>
2015-10-25 20:50:25 -0700

    delete the timer

--- Edison/main/include/drone_include.h
+++ Edison/main/include/drone_include.h
@@ -181,8 +181,6 @@ void update_g_arrd_yaw_pitch_roll()
 	MPU_init();
 	while (1)
 	{
-        printf("Timer started\n");
-        timer_start(&g_T_timer);
 		uint8_t Buf[14];
 		mraa_i2c_read_bytes_data(mpu, 59, Buf, 14);
 		// Accelerometer
@@ -235,9 +233,6 @@ void update_g_arrd_yaw_pitch_roll()
 		g_arrd_yaw_pitch_roll[1] = pitch;
 		g_arrd_yaw_pitch_roll[2] = roll;
 		//    printf("%.1f, %.1f, %.1f\n",yaw, pitch, roll);
-        timer_pause(&g_T_timer);
-        printf("Delta (us): %ld\n", timer_delta_us(&g_T_timer));
-        printf("Delta (ms): %ld\n", timer_delta_ms(&g_T_timer));
 	}
 }
 
