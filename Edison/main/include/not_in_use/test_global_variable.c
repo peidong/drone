ad2d11ce3b35097c4f381036522bf5c5e2c2a004
peidong <peidong@outlook.com>
2015-10-26 16:49:32 -0700

    global variable

--- Edison/main/include/not_in_use/test_global_variable.c
+++ Edison/main/include/not_in_use/test_global_variable.c
@@ -2,21 +2,23 @@
 
 struct T_test {
     int n1;
-    int n2;
+    int arrn[2];
 };
 
 struct T_test T_test1;
 
 void test(struct T_test *pT_test){
     pT_test->n1 = 100;
-    pT_test->n2 = 200;
+    pT_test->arrn[0] = 200;
+    pT_test->arrn[1] = 300;
 }
 
 int main(){
     T_test1.n1 = 9;
-    T_test1.n2 = 19;
-    printf("n1 = %d, n2 = %d", T_test1.n1, T_test1.n2);
+    T_test1.arrn[0] = 19;
+    T_test1.arrn[1] = 29;
+    printf("n1 = %d, arrn[0] = %d, arrn[1] = %d", T_test1.n1, T_test1.arrn[0], T_test1.arrn[1]);
     test(&T_test1);
-    printf("\nn1 = %d, n2 = %d", T_test1.n1, T_test1.n2);
+    printf("\nn1 = %d, arrn[0] = %d, arrn[1] = %d", T_test1.n1, T_test1.arrn[0], T_test1.arrn[1]);
     return 0;
 }
