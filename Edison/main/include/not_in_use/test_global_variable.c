400ed776f9eb22ad3b96596e5de8dfa942988b1e
peidong <peidong@outlook.com>
2015-10-27 13:45:52 -0700

    drone include update

--- Edison/main/include/not_in_use/test_global_variable.c
+++ Edison/main/include/not_in_use/test_global_variable.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 struct T_test {
     int n1;
@@ -23,9 +24,17 @@ void test(struct T_test *pT_test){
 }
 
 int main(){
+    struct timespec tspec_high;
+    tspec_high.tv_sec = 0;
+    tspec_high.tv_nsec = 500000000L;
     T_test1.n1 = 9;
     T_test1.arrn[0] = 19;
     T_test1.arrn[1] = 29;
+    while(1){
+        nanosleep(&tspec_high, NULL);
+        nanosleep(&tspec_high, NULL);
+        printf("1.5s\n");
+    }
     printf("n1 = %d, arrn[0] = %d, arrn[1] = %d", T_test1.n1, T_test1.arrn[0], T_test1.arrn[1]);
     test(&T_test1);
     printf("\nn1 = %d, arrn[0] = %d, arrn[1] = %d", T_test1.n1, T_test1.arrn[0], T_test1.arrn[1]);
