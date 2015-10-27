925b4f398e77fc7639cd25323a431999ab6e973d
peidong <peidong@outlook.com>
2015-10-26 18:02:25 -0700

    drone include

--- Edison/main/include/not_in_use/test_global_variable.c
+++ Edison/main/include/not_in_use/test_global_variable.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 struct T_test {
     int n1;
     int arrn[2];
+    char *str_2;
 };
 
 struct T_test T_test1;
@@ -11,6 +14,12 @@ void test(struct T_test *pT_test){
     pT_test->n1 = 100;
     pT_test->arrn[0] = 200;
     pT_test->arrn[1] = 300;
+    pT_test->str_2 = "Chen";
+    char *str_1 = "Peidong ";
+    char *str_3 = (char *) malloc(1 + strlen(str_1)+ strlen(pT_test->str_2));
+    strcpy(str_3, str_1);
+    strcat(str_3, pT_test->str_2);
+    printf("\n%s", str_3);
 }
 
 int main(){
