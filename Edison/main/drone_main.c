8d9b7cae7b32e9096e0fb79adbf6cfa557745d4b
peidong <peidong@outlook.com>
2015-10-27 14:37:26 -0700

    drone include update

--- Edison/main/drone_main.c
+++ Edison/main/drone_main.c
@@ -1,1 +1,12 @@
 #include "include/drone_include.h"
+
+int main()
+{
+    threadpool thpool = thpool_init(10);
+    thpool_add_work(thpool, (void*)ThreadTask_HTTP_get_pT_pwm, NULL);
+    thpool_add_work(thpool, (void*)ThreadTask_get_arrd_pwm, NULL);
+    thpool_wait(thpool);
+    thpool_destroy(thpool);
+    free_pT_pwm(g_pT_pwm);
+    return 0;
+}
