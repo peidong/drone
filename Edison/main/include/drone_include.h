c106e53f90bb3efbe2199b98077af85e8444e0dc
peidong <peidong@outlook.com>
2015-10-27 20:55:00 -0700

    drone include

--- Edison/main/include/drone_include.h
+++ Edison/main/include/drone_include.h
@@ -80,74 +80,76 @@ int HTTP_update_T_drone_pwm(struct T_drone *pT_drone){
     return 0;
 }
 
-struct T_hash_pwm* HTTP_get_pT_pwm()
-{
-    char *sz_url_get_pwm = "http://fryer.ee.ucla.edu/rest/api/pwm/get/";
-    /*char *sz_url_post_pwm = "http://fryer.ee.ucla.edu/rest/api/pwm/post/";*/
+/** old code with hash
+ */
+//struct T_hash_pwm* HTTP_get_pT_pwm()
+//{
+    //char *sz_url_get_pwm = "http://fryer.ee.ucla.edu/rest/api/pwm/get/";
+    //[>char *sz_url_post_pwm = "http://fryer.ee.ucla.edu/rest/api/pwm/post/";<]
     
-    char *sz_http_response;
-    struct json_object *pT_json_object_whole_response, *ppT_json_object_pwm[4], *pT_json_object_data, *pT_json_object_update_time;
-    int n_json_response;
-    double pd_pwm[4];
-    int n_index=0;
+    //char *sz_http_response;
+    //struct json_object *pT_json_object_whole_response, *ppT_json_object_pwm[4], *pT_json_object_data, *pT_json_object_update_time;
+    //int n_json_response;
+    //double pd_pwm[4];
+    //int n_index=0;
 
-    sz_http_response = http_get(sz_url_get_pwm);
+    //sz_http_response = http_get(sz_url_get_pwm);
 
-    pT_json_object_whole_response = json_tokener_parse(sz_http_response);
+    //pT_json_object_whole_response = json_tokener_parse(sz_http_response);
 
-    n_json_response = json_object_object_get_ex(pT_json_object_whole_response, "data", &pT_json_object_data);
-    n_json_response = json_object_object_get_ex(pT_json_object_data, "pwm1", &ppT_json_object_pwm[0]);
-    n_json_response = json_object_object_get_ex(pT_json_object_data, "pwm2", &ppT_json_object_pwm[1]);
-    n_json_response = json_object_object_get_ex(pT_json_object_data, "pwm3", &ppT_json_object_pwm[2]);
-    n_json_response = json_object_object_get_ex(pT_json_object_data, "pwm4", &ppT_json_object_pwm[3]);
-    n_json_response = json_object_object_get_ex(pT_json_object_data, "update_time", &pT_json_object_update_time);
+    //n_json_response = json_object_object_get_ex(pT_json_object_whole_response, "data", &pT_json_object_data);
+    //n_json_response = json_object_object_get_ex(pT_json_object_data, "pwm1", &ppT_json_object_pwm[0]);
+    //n_json_response = json_object_object_get_ex(pT_json_object_data, "pwm2", &ppT_json_object_pwm[1]);
+    //n_json_response = json_object_object_get_ex(pT_json_object_data, "pwm3", &ppT_json_object_pwm[2]);
+    //n_json_response = json_object_object_get_ex(pT_json_object_data, "pwm4", &ppT_json_object_pwm[3]);
+    //n_json_response = json_object_object_get_ex(pT_json_object_data, "update_time", &pT_json_object_update_time);
 
-    n_index = 0;
-    for(n_index = 0; n_index < 4; n_index++)
-    {
-        pd_pwm[n_index] = json_object_get_double(*(ppT_json_object_pwm+n_index));
-    }
+    //n_index = 0;
+    //for(n_index = 0; n_index < 4; n_index++)
+    //{
+        //pd_pwm[n_index] = json_object_get_double(*(ppT_json_object_pwm+n_index));
+    //}
 
-    const char **kppchIndex, *kstrKeys[] = {"pwm1", "pwm2", "pwm3", "pwm4", NULL};
-    struct T_hash_pwm *pT_pwm_selector, *pT_pwm_all = NULL;
+    //const char **kppchIndex, *kstrKeys[] = {"pwm1", "pwm2", "pwm3", "pwm4", NULL};
+    //struct T_hash_pwm *pT_pwm_selector, *pT_pwm_all = NULL;
 
-    n_index = 0;
-    for (kppchIndex = kstrKeys; *kppchIndex != NULL; kppchIndex++) {
-        pT_pwm_selector = (struct T_hash_pwm*)malloc(sizeof(struct T_hash_pwm));
-        if (pT_pwm_selector == NULL) {
-            exit(-1);
-        }
-        pT_pwm_selector->pstr_key = *kppchIndex;
-        pT_pwm_selector->d_pwm = pd_pwm[n_index];
-        n_index++;
-        HASH_ADD_KEYPTR(hh, pT_pwm_all, pT_pwm_selector->pstr_key, strlen(pT_pwm_selector->pstr_key), pT_pwm_selector);
-    }
+    //n_index = 0;
+    //for (kppchIndex = kstrKeys; *kppchIndex != NULL; kppchIndex++) {
+        //pT_pwm_selector = (struct T_hash_pwm*)malloc(sizeof(struct T_hash_pwm));
+        //if (pT_pwm_selector == NULL) {
+            //exit(-1);
+        //}
+        //pT_pwm_selector->pstr_key = *kppchIndex;
+        //pT_pwm_selector->d_pwm = pd_pwm[n_index];
+        //n_index++;
+        //HASH_ADD_KEYPTR(hh, pT_pwm_all, pT_pwm_selector->pstr_key, strlen(pT_pwm_selector->pstr_key), pT_pwm_selector);
+    //}
 
-    return pT_pwm_all;
-}
+    //return pT_pwm_all;
+//}
 
-int free_pT_pwm(struct T_hash_pwm *pT_pwm_all){
-    struct T_hash_pwm *pT_pwm_selector, *pT_pwm_tmp;
-    /* free the hash table contents */
-    HASH_ITER(hh, pT_pwm_all, pT_pwm_selector, pT_pwm_tmp) {
-        HASH_DEL(pT_pwm_all, pT_pwm_selector);
-        free(pT_pwm_selector);
-    }
-    return 0;
-} 
+//int free_pT_pwm(struct T_hash_pwm *pT_pwm_all){
+    //struct T_hash_pwm *pT_pwm_selector, *pT_pwm_tmp;
+    //[> free the hash table contents <]
+    //HASH_ITER(hh, pT_pwm_all, pT_pwm_selector, pT_pwm_tmp) {
+        //HASH_DEL(pT_pwm_all, pT_pwm_selector);
+        //free(pT_pwm_selector);
+    //}
+    //return 0;
+//} 
 
-double get_d_pwm(struct T_hash_pwm *pT_pwm_all, char *sz_pwm_key)
-{
-    struct T_hash_pwm *pT_pwm_selector;
-    double d_pwm;
-    HASH_FIND_STR(pT_pwm_all, sz_pwm_key, pT_pwm_selector);
-    if (pT_pwm_selector != NULL) {
-        d_pwm = pT_pwm_selector->d_pwm;
-    }else{
-        d_pwm = 0;
-    }
-    return d_pwm;
-}
+//double get_d_pwm(struct T_hash_pwm *pT_pwm_all, char *sz_pwm_key)
+//{
+    //struct T_hash_pwm *pT_pwm_selector;
+    //double d_pwm;
+    //HASH_FIND_STR(pT_pwm_all, sz_pwm_key, pT_pwm_selector);
+    //if (pT_pwm_selector != NULL) {
+        //d_pwm = pT_pwm_selector->d_pwm;
+    //}else{
+        //d_pwm = 0;
+    //}
+    //return d_pwm;
+//}
 
 int HTTP_update_T_control(struct T_control *pT_control){
     //How to concat two char* string in C program
--- Edison/only_for_test/pwm/pwm.c
+++ Edison/only_for_test/pwm/pwm.c
@@ -26,32 +26,32 @@ int main()
     int n_index;
     while(1)
     {
-        g_pT_hash_pwm = HTTP_get_pT_pwm();
-        g_arrd_current_pwm[0] = get_d_pwm(g_pT_hash_pwm, "pwm1") / 1000;
-        g_arrd_current_pwm[1] = get_d_pwm(g_pT_hash_pwm, "pwm2") / 1000;
-        g_arrd_current_pwm[2] = get_d_pwm(g_pT_hash_pwm, "pwm3") / 1000;
-        g_arrd_current_pwm[3] = get_d_pwm(g_pT_hash_pwm, "pwm4") / 1000;
-        if (g_arrd_last_pwm[0] != g_arrd_current_pwm[0]){
-            mraa_pwm_write(pwm1, (g_arrd_current_pwm[0]));
+        HTTP_update_T_drone_pwm(&g_T_drone_my);
+        g_T_drone_my.arrd_current_pwm[0] = get_d_pwm(g_pT_hash_pwm, "pwm1") / 1000;
+        g_T_drone_my.arrd_current_pwm[1] = get_d_pwm(g_pT_hash_pwm, "pwm2") / 1000;
+        g_T_drone_my.arrd_current_pwm[2] = get_d_pwm(g_pT_hash_pwm, "pwm3") / 1000;
+        g_T_drone_my.arrd_current_pwm[3] = get_d_pwm(g_pT_hash_pwm, "pwm4") / 1000;
+        if (g_arrd_last_pwm[0] != g_T_drone_my.arrd_current_pwm[0]){
+            mraa_pwm_write(pwm1, (g_T_drone_my.arrd_current_pwm[0]));
         }
-        if (g_arrd_last_pwm[1] != g_arrd_current_pwm[1]){
-            mraa_pwm_write(pwm2, (g_arrd_current_pwm[1]));
+        if (g_arrd_last_pwm[1] != g_T_drone_my.arrd_current_pwm[1]){
+            mraa_pwm_write(pwm2, (g_T_drone_my.arrd_current_pwm[1]));
         }
-        if (g_arrd_last_pwm[2] != g_arrd_current_pwm[2]){
-            mraa_pwm_write(pwm3, (g_arrd_current_pwm[2]));
+        if (g_arrd_last_pwm[2] != g_T_drone_my.arrd_current_pwm[2]){
+            mraa_pwm_write(pwm3, (g_T_drone_my.arrd_current_pwm[2]));
         }
-        if (g_arrd_last_pwm[3] != g_arrd_current_pwm[3]){
-            mraa_pwm_write(pwm4, (g_arrd_current_pwm[3]));
+        if (g_arrd_last_pwm[3] != g_T_drone_my.arrd_current_pwm[3]){
+            mraa_pwm_write(pwm4, (g_T_drone_my.arrd_current_pwm[3]));
         }
-        g_arrd_last_pwm[0] = g_arrd_current_pwm[0];
-        g_arrd_last_pwm[1] = g_arrd_current_pwm[1];
-        g_arrd_last_pwm[2] = g_arrd_current_pwm[2];
-        g_arrd_last_pwm[3] = g_arrd_current_pwm[3];
+        g_arrd_last_pwm[0] = g_T_drone_my.arrd_current_pwm[0];
+        g_arrd_last_pwm[1] = g_T_drone_my.arrd_current_pwm[1];
+        g_arrd_last_pwm[2] = g_T_drone_my.arrd_current_pwm[2];
+        g_arrd_last_pwm[3] = g_T_drone_my.arrd_current_pwm[3];
         usleep(50000);
-        printf("pwm1 = %f\n", (g_arrd_current_pwm[0]));
-        printf("pwm2 = %f\n", (g_arrd_current_pwm[1]));
-        printf("pwm3 = %f\n", (g_arrd_current_pwm[2]));
-        printf("pwm4 = %f\n\n", (g_arrd_current_pwm[3]));
+        printf("pwm1 = %f\n", (g_T_drone_my.arrd_current_pwm[0]));
+        printf("pwm2 = %f\n", (g_T_drone_my.arrd_current_pwm[1]));
+        printf("pwm3 = %f\n", (g_T_drone_my.arrd_current_pwm[2]));
+        printf("pwm4 = %f\n\n", (g_T_drone_my.arrd_current_pwm[3]));
         /*float output = mraa_pwm_read(pwm1);*/
     }
     return 0;
