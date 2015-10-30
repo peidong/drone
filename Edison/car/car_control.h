362a522c7177c4077961f1fa902e988263f2d3e9
byyangyang <byyangyang@ucla.edu>
2015-10-30 16:56:24 -0700

    compute distance

--- Edison/car/car_control.h
+++ Edison/car/car_control.h
@@ -8,6 +8,7 @@
 #define LEFTMAX 0.048f
 #define RIGHTMAX 0.088f
 
+const double EARTH_RADIUS = 6378.137;
 
 float speed, turn;
 mraa_pwm_context speed_pwm_in1, speed_pwm_in2, turn_pwm;
@@ -55,3 +56,34 @@ void move_backward(){
     speed_control(speed_pwm_in1, speed_pwm_in2, speed);
     
 }
+//compute latitude and longitude distance
+static double rad(double d)
+{
+    return d * PI/180.0;
+}
+
+static double GetLatDistance(double lat1, double lat2, double lon1)
+{
+    double radLat1 = rad(lat1);   
+    double radLat2 = rad(lat2);   
+    double a = radLat1 - radLat2;   
+    double b = rad(lon1) - rad(lon1);   
+    double s = 2 * sin(sqrt(pow(sin(a/2),2) + cos(radLat1)*cos(radLat2)*pow(sin(b/2),2)));   
+    s = s * EARTH_RADIUS;   
+    s = round(s * 10000) / 10000;   
+    return s;   
+
+}
+
+static double GetLonDistance(double lon1, double lon2, double lat1)
+{
+    double radLat1 = rad(lat1);   
+    double radLat2 = rad(lat1);   
+    double a = radLat1 - radLat2;   
+    double b = rad(lon1) - rad(lon2);   
+    double s = 2 * sin(sqrt(pow(sin(a/2),2) + cos(radLat1)*cos(radLat2)*pow(sin(b/2),2)));   
+    s = s * EARTH_RADIUS;   
+    s = round(s * 10000) / 10000;   
+    return s;  
+}
+
