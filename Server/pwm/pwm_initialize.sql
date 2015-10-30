8a7c91978505cd743ab2fe780ad69d48d6aa811b
peidong <peidong@outlook.com>
2015-10-30 16:44:46 -0700

    pwm

--- Server/pwm/pwm_initialize.sql
+++ Server/pwm/pwm_initialize.sql
@@ -3,10 +3,10 @@ USE edison;
 DROP TABLE IF EXISTS edison.pwm;
 CREATE TABLE IF NOT EXISTS edison.pwm (
     `id` int(11) NOT NULL DEFAULT 0,
-    `pwm1` double(5,2) DEFAULT NULL,
-    `pwm2` double(5,2) DEFAULT NULL,
-    `pwm3` double(5,2) DEFAULT NULL,
-    `pwm4` double(5,2) DEFAULT NULL,
+    `pwm1` double(8,7) DEFAULT NULL,
+    `pwm2` double(8,7) DEFAULT NULL,
+    `pwm3` double(8,7) DEFAULT NULL,
+    `pwm4` double(8,7) DEFAULT NULL,
     `update_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
     ) ENGINE = InnoDB;
 
