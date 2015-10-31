f65dd0f5b61bd9ac4622a628c2afcb398a2f9e10
byyangyang <byyangyang@ucla.edu>
2015-10-30 17:21:38 -0700

    car http get remain to be done

--- Edison/car/car_control.h
+++ Edison/car/car_control.h
@@ -56,7 +56,59 @@ void move_backward(){
     speed_control(speed_pwm_in1, speed_pwm_in2, speed);
     
 }
-//compute latitude and longitude distance
+/*
+ * This part is for HTTP GET
+ * We get the longitude and direction from server
+ */
+
+int update_T_drone_http(struct T_drone *pT_drone){
+    //How to concat two char* string in C program
+    //http://stackoverflow.com/questions/18468229/how-to-concat-two-char-string-in-c-program
+    //sz_mac_address = "fc:c2:de:3d:7f:af";
+    char *sz_url_get_control_part1 = "http://fryer.ee.ucla.edu/rest/api/control/get/?mac_address=";
+    char *sz_url_get_control = (char*) malloc(1 + strlen(sz_url_get_control_part1) + strlen(pT_drone->sz_mac_address));
+    strcpy(sz_url_get_control, sz_url_get_control_part1);
+    strcat(sz_url_get_control, pT_drone->sz_mac_address);
+    /*char *sz_url_post_control = "http://fryer.ee.ucla.edu/rest/api/control/post/";*/
+    
+    char *sz_http_response;
+    struct json_object *pT_json_object_whole_response, *ppT_json_object_suspend_pwm[4], *pT_json_object_data, *pT_json_object_update_time, *pT_json_object_control_type, *pT_json_object_auto_control_command, *pT_json_object_manual_control_command;
+    int n_json_response;
+    int n_index=0;
+
+    sz_http_response = http_get(sz_url_get_control);
+
+    pT_json_object_whole_response = json_tokener_parse(sz_http_response);
+
+    n_json_response = json_object_object_get_ex(pT_json_object_whole_response, "data", &pT_json_object_data);
+    n_json_response = json_object_object_get_ex(pT_json_object_data, "control_type", &pT_json_object_control_type);
+    n_json_response = json_object_object_get_ex(pT_json_object_data, "auto_control_command", &pT_json_object_auto_control_command);
+    n_json_response = json_object_object_get_ex(pT_json_object_data, "manual_control_command", &pT_json_object_manual_control_command);
+    n_json_response = json_object_object_get_ex(pT_json_object_data, "suspend_pwm1", &ppT_json_object_suspend_pwm[0]);
+    n_json_response = json_object_object_get_ex(pT_json_object_data, "suspend_pwm2", &ppT_json_object_suspend_pwm[1]);
+    n_json_response = json_object_object_get_ex(pT_json_object_data, "suspend_pwm3", &ppT_json_object_suspend_pwm[2]);
+    n_json_response = json_object_object_get_ex(pT_json_object_data, "suspend_pwm4", &ppT_json_object_suspend_pwm[3]);
+    n_json_response = json_object_object_get_ex(pT_json_object_data,"update_time",&pT_json_object_update_time);
+
+    pT_drone->n_control_type = json_object_get_int(pT_json_object_control_type);
+    pT_drone->n_auto_control_command = json_object_get_int(pT_json_object_auto_control_command);
+    pT_drone->n_manual_control_command = json_object_get_int(pT_json_object_manual_control_command);
+    for(n_index = 0; n_index < 4; n_index++)
+    {
+        pT_drone->arrd_suspend_pwm[n_index] = json_object_get_double(*(ppT_json_object_suspend_pwm + n_index));
+    }
+    return 0;
+}
+
+
+
+
+/*
+ * This part is for compute latitude and longitude distance
+ * This two distance are computed separaterly
+ * The latitude distance is for north and south
+ * The longitude distance is for east and west
+ */
 static double rad(double d)
 {
     return d * PI/180.0;
