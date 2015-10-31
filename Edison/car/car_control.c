b4daac22a01d177f29c8f410fc80ad723210cb34
byyangyang <byyangyang@ucla.edu>
2015-10-31 13:40:07 -0700

    car update

--- Edison/car/car_control.c
+++ Edison/car/car_control.c
@@ -19,47 +19,47 @@ int main(){
 	mraa_pwm_write(speed_pwm_in1, 1.0f);
 	mraa_pwm_write(speed_pwm_in2, 1.0f);
 
-    /*threadpool thpool = thpool_init(10);*/
-    /*thpool_add_work(thpool, (void*)ThreadTask_update_T_drone_http_gps, (void*)&g_T_drone_self);*/
-    /*thpool_add_work(thpool, (void*)ThreadTask_GpsNavigationMove, (void*)&g_T_drone_self);*/
-    /*thpool_wait(thpool);*/
-    /*thpool_destroy(thpool);*/
+    threadpool thpool = thpool_init(10);
+    thpool_add_work(thpool, (void*)ThreadTask_update_T_drone_http_gps, (void*)&g_T_drone_self);
+    thpool_add_work(thpool, (void*)ThreadTask_GpsNavigationMove, (void*)&g_T_drone_self);
+    thpool_wait(thpool);
+    thpool_destroy(thpool);
 
-     while(1){
-             printf("Turn (L, C, R): ");                                      
-             scanf("%s", turn_user_input);               
+     /*while(1){*/
+             /*printf("Turn (L, C, R): ");                                      */
+             /*scanf("%s", turn_user_input);               */
                 
-             if (!strcmp(turn_user_input, "L") || !strcmp(turn_user_input, "l"))     
-             {
-                 for (i = 0;i<=180;i++){
-                     turn_left();
-                 }
-             }else if (!strcmp(turn_user_input, "C") || !strcmp(turn_user_input, "c"))
-             {
-                 for (i = 0;i<=50;i++){
-                     move_forward(0.0);
-                 }
-             }               
-             else if (!strcmp(turn_user_input, "R") || !strcmp(turn_user_input, "r")){
-                 for (i = 0;i<=180;i++){
-                     turn_right();
-                 } 
-             }
-             else if(!strcmp(turn_user_input, "S") || !strcmp(turn_user_input, "s"))
-             {
-                  f_turn = CENTER;
-                  mraa_pwm_write(turn_pwm, f_turn);
-                  usleep(10000);
-                  speed_control(speed_pwm_in1, speed_pwm_in2, 0);
-             }
-             else {                                 
-                     printf("Wrong turn type!\n");
-              return 1;
-         }
+             /*if (!strcmp(turn_user_input, "L") || !strcmp(turn_user_input, "l"))     */
+             /*{*/
+                 /*for (i = 0;i<=180;i++){*/
+                     /*turn_left();*/
+                 /*}*/
+             /*}else if (!strcmp(turn_user_input, "C") || !strcmp(turn_user_input, "c"))*/
+             /*{*/
+                 /*for (i = 0;i<=50;i++){*/
+                     /*move_forward(0.0);*/
+                 /*}*/
+             /*}               */
+             /*else if (!strcmp(turn_user_input, "R") || !strcmp(turn_user_input, "r")){*/
+                 /*for (i = 0;i<=180;i++){*/
+                     /*turn_right();*/
+                 /*} */
+             /*}*/
+             /*else if(!strcmp(turn_user_input, "S") || !strcmp(turn_user_input, "s"))*/
+             /*{*/
+                  /*f_turn = CENTER;*/
+                  /*mraa_pwm_write(turn_pwm, f_turn);*/
+                  /*usleep(10000);*/
+                  /*speed_control(speed_pwm_in1, speed_pwm_in2, 0);*/
+             /*}*/
+             /*else {                                 */
+                     /*printf("Wrong turn type!\n");*/
+              /*return 1;*/
+         /*}*/
         
-         sleep(1);
-         speed_control(speed_pwm_in1, speed_pwm_in2, 0.0f);
-     } 
+         /*sleep(1);*/
+         /*speed_control(speed_pwm_in1, speed_pwm_in2, 0.0f);*/
+     /*} */
 
 
     return 0;
