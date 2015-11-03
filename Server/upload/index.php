2eafc0e3880839471aa77d873f9536c33e8e6ae4
peidong <peidong@outlook.com>
2015-11-02 18:29:59 -0800

    send image

--- Server/upload/index.php
+++ Server/upload/index.php
@@ -1,9 +1,19 @@
 <?php
-    $uploadpath = "files/";
-    $filedata = $_FILES['file']['tmp_name'];
-    echo "filedata= ".$filedata;
-    if ($filedata != ''){
-        copy($filedata,$uploadpath."1.c");
-        echo "success";
+    $targetfolder = "files/";
+    $targetfolder = $targetfolder.basename($_FILES['file']['name']);
+    if(move_uploaded_file($_FILES['file']['tmp_name'], $targetfolder))
+    {
+        echo "The file ".basename($_FILES['file']['name'])." is uploaded";
     }
+    else{
+        echo "Problem uploading file";
+    } 
+
+    //$uploadpath = "files/";
+    //$filedata = $_FILES['file']['tmp_name'];
+    //echo "filedata= ".$filedata;
+    //if ($filedata != ''){
+        //copy($filedata,$uploadpath."1.c");
+        //echo "success";
+    //}
 ?>
