d37a5822f3adb2996a3aa4244710a47a819009c5
peidong <peidong@outlook.com>
2015-11-02 20:03:25 -0800

    send image

--- Edison/main/python/send_image.py
+++ Edison/main/python/send_image.py
@@ -4,13 +4,24 @@ import datetime
 import json
 import yaml#pip install pyyaml
 import threading
+import requests#sudo pip install requests
 
-def capture_image(str_filename):
+def get_str_filename():
+    str_filename = str(datetime.datetime.now())
+    str_filename = str_filename.replace(' ','-')
+    str_filename = "files/" + str_filename + ".jpg"
+    return str_filename
+
+def CaptureImage(str_filename):
     capture = cv.CaptureFromCAM(0)
     img = cv.QueryFrame(capture)
-    cv.SaveImage("files/"+str_filename+".jpg", img)
+    cv.SaveImage(str_filename, img)
 
-capture_image(str(datetime.datetime.now()))
-# print datetime.datetime.now()
+def UploadFile(str_filename, str_url):
+    files = {'file': open(str_filename)}
+    response = requests.post(str_url, files=files)
 
 
+str_filename = get_str_filename()
+CaptureImage(str_filename)
+UploadFile(str_filename, "http://fryer.ee.ucla.edu/rest/api/upload/")
--- Server/upload/index.php
+++ Server/upload/index.php
@@ -1,4 +1,5 @@
 <?php
+//http://www.tutorialscollection.com/php-file-upload-how-to-upload-file-in-php-using-_files-in-script-of-php/
     $targetfolder = "files/";
     $targetfolder = $targetfolder.basename($_FILES['file']['name']);
     if(move_uploaded_file($_FILES['file']['tmp_name'], $targetfolder))
@@ -7,7 +8,7 @@
     }
     else{
         echo "Problem uploading file";
-    } 
+    }
 
     //$uploadpath = "files/";
     //$filedata = $_FILES['file']['tmp_name'];
