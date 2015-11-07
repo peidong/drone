ad197a3137b9e0b19bdc9cf9c4b3203ea277a4b6
peidong <peidong@outlook.com>
2015-11-07 11:20:41 -0800

    upload file server

--- Server/opencv/CMakeFiles/symbol_detection.dir/cmake_clean.cmake
+++ Server/opencv/CMakeFiles/symbol_detection.dir/cmake_clean.cmake
@@ -1,10 +1,10 @@
-file(REMOVE_RECURSE
+FILE(REMOVE_RECURSE
   "CMakeFiles/symbol_detection.dir/symbol_detection.cpp.o"
   "symbol_detection.pdb"
   "symbol_detection"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/symbol_detection.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/symbol_detection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
