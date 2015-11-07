e4c547fb3fc7966a2b1e42f367ef590483168d02
peidong <peidong@outlook.com>
2015-11-06 18:34:37 -0800

    upload files

--- Server/upload/index.php
+++ Server/upload/index.php
@@ -1,5 +1,7 @@
 <?php
 //http://www.tutorialscollection.com/php-file-upload-how-to-upload-file-in-php-using-_files-in-script-of-php/
+    // start the session
+    session_start();
     $targetfolder = "files/";
     $targetfolder = $targetfolder.basename($_FILES['file']['name']);
     if(move_uploaded_file($_FILES['file']['tmp_name'], $targetfolder))
@@ -9,6 +11,8 @@
     else{
         echo "Problem uploading file";
     }
+    // close the session
+    session_write_close();
 
     //$uploadpath = "files/";
     //$filedata = $_FILES['file']['tmp_name'];
