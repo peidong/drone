24e977a3c2f1f4fd251ecdf27b60360c1a339c95
byyangyang <byyangyang@ucla.edu>
2015-11-06 19:11:13 -0800

    car set

--- Edison/car/car_control.c
+++ Edison/car/car_control.c
@@ -5,8 +5,8 @@ int main(){
     g_T_drone_self.nflag_stop_all = 0;
     speed_pwm_in1 = mraa_pwm_init(3);
     speed_pwm_in2 = mraa_pwm_init(5);
-    turn_pwm = mraa_pwm_init(6);
-    sonicTurn_pwm = mraa_pwm_init(9);
+    turn_pwm = mraa_pwm_init(9);
+    sonicTurn_pwm = mraa_pwm_init(6);
     
     char turn_user_input[MAXBUFSIZ];
     f_speed = 40;
