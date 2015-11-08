c2b17e697a306c37e4973487935ba9f956f9ab0d
byyangyang <byyangyang@ucla.edu>
2015-11-08 12:23:06 -0800

    car debug

--- Edison/car/car_control.c
+++ Edison/car/car_control.c
@@ -42,13 +42,13 @@ int main(){
 	speed_control(speed_pwm_in1, speed_pwm_in2, 100.0f);
 
 
-    threadpool thpool = thpool_init(10);
-    thpool_add_work(thpool, (void*)ThreadTask_update_T_drone_http_gps, (void*)&g_T_drone_self);
-    thpool_add_work(thpool, (void*)ThreadTask_update_T_drone_http, (void*)&g_T_drone_self);
-    thpool_add_work(thpool, (void*)ThreadTask_sonicTurn_pwm, (void*)&g_T_drone_self);
-    thpool_add_work(thpool, (void*)ThreadTask_manual_control, (void*)&g_T_drone_self);
-    thpool_wait(thpool);
-    thpool_destroy(thpool);
+    /*threadpool thpool = thpool_init(10);*/
+    /*thpool_add_work(thpool, (void*)ThreadTask_update_T_drone_http_gps, (void*)&g_T_drone_self);*/
+    /*thpool_add_work(thpool, (void*)ThreadTask_update_T_drone_http, (void*)&g_T_drone_self);*/
+    /*thpool_add_work(thpool, (void*)ThreadTask_sonicTurn_pwm, (void*)&g_T_drone_self);*/
+    /*thpool_add_work(thpool, (void*)ThreadTask_manual_control, (void*)&g_T_drone_self);*/
+    /*thpool_wait(thpool);*/
+    /*thpool_destroy(thpool);*/
 
  
     g_f_turn = CENTER;
