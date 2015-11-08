fd30399cdd885229fd501789e3b7983f38d7cc0c
peidong <peidong@outlook.com>
2015-11-08 15:15:00 -0800

    small updates

--- Edison/only_for_test/old_code/ss/mpu.c
+++ Edison/only_for_test/old_code/ss/mpu.c
@@ -63,7 +63,8 @@ clock_t now=0,past=0;
 float beta=1.5f;  // compute beta
 
 float pitch, yaw, roll;
-float deltat = 0.0015f;                             // integration interval for both filter schemes
+float deltat = 0.001f;                             // integration interval for both filter schemes
+// float deltat = 0.0015f;                             // integration interval for both filter schemes
 int lastUpdate = 0, firstUpdate = 0, Now = 0;    // used to calculate integration interval                               // used to calculate integration interval
 float q[4] = {1.0f, 0.0f, 0.0f, 0.0f};           // vector to hold quaternion
 //--------------------------------
