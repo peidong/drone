f59ad9acc691d2282a7f8b0586407565b8f3181d
byyangyang <byyangyang@ucla.edu>
2015-11-07 18:40:28 -0800

    car debug

--- Edison/car/car_control.c
+++ Edison/car/car_control.c
@@ -31,7 +31,7 @@ int main(){
     // thpool_add_work(thpool, (void*)ThreadTask_update_T_drone_http_gps, (void*)&g_T_drone_self);
     thpool_add_work(thpool, (void*)ThreadTask_update_T_drone_http, (void*)&g_T_drone_self);
     // thpool_add_work(thpool, (void*)ThreadTask_sonicTurn_pwm, (void*)&g_T_drone_self);
-    // thpool_add_work(thpool, (void*)ThreadTask_GpsNavigationMove, (void*)&g_T_drone_self);
+     thpool_add_work(thpool, (void*)ThreadTask_GpsNavigationMove, (void*)&g_T_drone_self);
     /*thpool_add_work(thpool, (void*)ThreadTask_Ultrasonic_read_left, (void*)&g_T_drone_self);*/
     // thpool_add_work(thpool, (void*)ThreadTask_Ultrasonic_read_right, (void*)&g_T_drone_self);
     /*thpool_add_work(thpool, (void*)ThreadTask_Ultrasonic_read_center, (void*)&g_T_drone_self);*/
