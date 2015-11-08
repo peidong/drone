b2672fd47300a618d1102e66e0d10ca2a9c5657e
peidong <peidong@outlook.com>
2015-11-07 18:46:37 -0800

    pwm

--- Edison/main/include/drone_include.h
+++ Edison/main/include/drone_include.h
@@ -21,7 +21,7 @@
 #define PRINT_DEBUG_PID_CHANGE
 // #define PRINT_DEBUG_PID_TUNING
 // #define PRINT_DEBUG_PWM
-#define PRINT_DEBUG_THREAD
+// #define PRINT_DEBUG_THREAD
 // #define PRINT_CAR_MANUAL
 
 /**
