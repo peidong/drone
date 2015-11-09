387f636252939f70d9b46649d1cb1cb12a0e195a
peidong <peidong@outlook.com>
2015-11-09 13:06:53 -0800

    car

--- Edison/car/car_control.h
+++ Edison/car/car_control.h
@@ -319,9 +319,9 @@ int GpsNavigationMove(struct T_drone *pT_drone){
     if (tmp > 10)
     {
         //turn_direction(pT_drone->d_move_direction - pT_drone->d_face_direction);
-        if(pT_drone->d_move_direction - pT_drone->d_face_direction > 5){
+        if(pT_drone->d_move_direction - pT_drone->d_face_direction > 15){
             turn_right();
-        }else if(pT_drone->d_move_direction - pT_drone->d_face_direction < -5)
+        }else if(pT_drone->d_move_direction - pT_drone->d_face_direction < -15)
         {   
             turn_left();
         }
