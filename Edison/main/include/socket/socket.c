5c6437dd5138aa4d202fcfefdc0ee7cb06435e92
peidong <peidong@outlook.com>
2015-11-17 13:54:39 -0800

    socket

--- Edison/main/include/socket/socket.c
+++ Edison/main/include/socket/socket.c
@@ -3,6 +3,16 @@
 #include<sys/socket.h>
 #include<arpa/inet.h> //inet_addr
 
+/* Description of data base entry for a single host.  */
+struct hostent
+{
+  char *h_name;         /* Official name of host.  */
+  char **h_aliases;     /* Alias list.  */
+  int h_addrtype;       /* Host address type.  */
+  int h_length;         /* Length of address.  */
+  char **h_addr_list;       /* List of addresses from name server.  */
+};
+
 int main(int argc , char *argv[])
 {
     int socket_desc;
