cdd90d2545381fe4f3d2a8d98c263837c7b0f7bc
peidong <peidong@outlook.com>
2015-11-20 22:55:42 -0800

    small changes

--- Edison/main/include/drone_include.h
+++ Edison/main/include/drone_include.h
@@ -505,7 +505,7 @@ int update_T_drone_arrd_yaw_pitch_roll(struct T_drone *pT_drone){
     int16_t mrawx,mrawy,mrawz;
     float ax,ay,az,gx,gy,gz,mx,my,mz;
     float yaw, pitch, roll;
-	float result[201][3]; 
+	float result[20001][3]; 
 	int sample = 0;
 
     mraa_uart_context uno;
@@ -563,20 +563,20 @@ int update_T_drone_arrd_yaw_pitch_roll(struct T_drone *pT_drone){
 
             printf("%.1f\t%.1f\t%.1f\n", mx, my, mz);
             
-if(sample < 200)
+if(sample < 20000)
 	{
 		result[sample][0] = mx;
     	result[sample][1] = my;
     	result[sample][2] = mz;
     	sample++;
 	}
-if(sample == 200)
+if(sample == 20000)
 	{
 		printf("Outputing data of mag!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 		FILE* fp;
 		int i,j;
 		fp = fopen("demo.txt", "w");
-		for (i = 0; i < 200; i++)
+		for (i = 0; i < 20000; i++)
 		{
 		    for (j = 0; j < 3; j++)
 		    {
@@ -585,8 +585,8 @@ if(sample == 200)
 		    fputc('\n', fp);
 		}
 		fclose(fp);
-		printf("Finish!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-		sample = 201;
+		printf("Finish!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+		sample = 20001;
 	}
 
 
