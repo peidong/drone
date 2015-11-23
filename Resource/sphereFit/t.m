fbb070f6d1b16875fa93f75ca6e7c9fe3234f548
peidong <peidong@outlook.com>
2015-11-23 13:13:07 -0800

    document update

---
+++ Resource/sphereFit/t.m
@@ -0,0 +1,6 @@
+demo=importdata('/Users/peidong/Documents/SyncFolders/Dropbox/UCLA/2015Fall/EE202C/Project/document/sphereFit/demo.txt');
+x = demo(:,1);
+y = demo(:,2);
+z = demo(:,3);
+
+sphereFit(demo)
