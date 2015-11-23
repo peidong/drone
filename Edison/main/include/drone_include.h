c2c5b41eeaaeb903177a2a79ac95894a5a3dc54c
peidong <peidong@outlook.com>
2015-11-22 22:59:35 -0800

    all the threads

--- Edison/main/include/drone_include.h
+++ Edison/main/include/drone_include.h
@@ -921,7 +921,7 @@ int update_T_drone_arrd_pid(struct T_drone *pT_drone){
 #ifdef  PRINT_DEBUG_PID_CHANGE
         printf("pitch change= %f\troll change= %f\n",(pT_drone->arrd_pid_yaw_pitch_roll[1] / 2), (pT_drone->arrd_pid_yaw_pitch_roll[2] / 2));
 #endif
-        usleep(100000); // We need to add some delay to slow down the pid loop. Mainly, 100ms cycle should be good.
+        //usleep(100000); // We need to add some delay to slow down the pid loop. Mainly, 100ms cycle should be good.
 #ifdef TIMER_PID
         timer_pause(&g_timer);
         printf("Delta (us): %ld\n", timer_delta_us(&g_timer) - g_last_time_us);
