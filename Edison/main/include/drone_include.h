61195a70d34724eab7a83a5a9449a0821c7942d3
peidong <peidong@outlook.com>
2015-11-25 17:28:00 -0800

    delay time for http

--- Edison/main/include/drone_include.h
+++ Edison/main/include/drone_include.h
@@ -964,7 +964,7 @@ int update_T_drone_arrd_pid(struct T_drone *pT_drone){
 
         Pid_Run(pidData_second_yaw, pT_drone->n_grawz/32768.0);
         Pid_Run(pidData_second_pitch, pT_drone->n_grawy/32768.0);
-        Pid_Run(pidData_second_roll, pT_drone->n_grawx/32768.0);
+        Pid_Run(pidData_second_roll, pT_drone->n_grawx/32768.0 - 0.001);
 
         d_second_yaw = pidData_second_yaw->output;
         d_second_pitch = pidData_second_pitch->output;
@@ -1024,7 +1024,7 @@ int update_T_drone_arrd_pid(struct T_drone *pT_drone){
             }
         }
 #ifdef  PRINT_DEBUG_PID_CHANGE
-        printf("first roll= %f\tsecond roll= %f\traw= %f\n",(d_rate_roll), (d_second_roll/200), pT_drone->n_grawx/32768.0);
+        printf("first roll= %f\tsecond roll= %f\traw= %f\n",(d_rate_roll), (d_second_roll/200), pT_drone->n_grawx/32768.0 - 0.001);
 #endif
         usleep(PID_SLEEP_US); // We need to add some delay to slow down the pid loop. Mainly, 100ms cycle should be good.
 #ifdef TIMER_PID
