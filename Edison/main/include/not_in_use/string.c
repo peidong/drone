e766ffe0ec464a8a42a348f87764769ecb73edea
peidong <peidong@outlook.com>
2015-12-03 14:00:33 -0800

    README

--- Edison/main/include/not_in_use/string.c
+++ Edison/main/include/not_in_use/string.c
@@ -3,18 +3,6 @@
 #include <stdio.h>   /* printf */
 
 int main(){
-    int n = 1234;
-    char arrc_test[8];
-    arrc_test[0]='-';
-    arrc_test[1]='9';
-    arrc_test[2]='3';
-    arrc_test[3]='a';
-    n = atoi(arrc_test);
-    printf("n=%d\n", n);
-    /*sprintf(arrc_test, "%d", n);*/
-    /*printf("arrc_test[0] = %c\n", arrc_test[0]);*/
-    /*printf("arrc_test[1] = %c\n", arrc_test[1]);*/
-    /*printf("arrc_test[2] = %c\n", arrc_test[2]);*/
-    /*printf("arrc_test[3] = %c\n", arrc_test[3]);*/
+    printf("%f\n", atof("0.012000"));
 	return 0;
 }
