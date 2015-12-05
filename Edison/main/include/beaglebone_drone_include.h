1682b25aea9b9baa130f3b92a0cde53a78b8cb20
peidong <peidong@outlook.com>
2015-12-04 18:53:50 -0800

    pwm

--- Edison/main/include/beaglebone_drone_include.h
+++ Edison/main/include/beaglebone_drone_include.h
@@ -37,7 +37,7 @@
 #define PWM_MANUAL_CHANGE_AMOUNT_LARGE 0.000025*80
 #define PWM_INITIAL 0.000025*4
 #define PWM_MIN 0.000025*1000
-#define PWM_RANGE 0.000025*500
+#define PWM_RANGE 0.000025*2500
 
 int n_index_yaw_pitch_roll = 0;
 #ifdef TIMER
