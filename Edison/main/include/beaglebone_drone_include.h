470158dd1aac72a0ca9282c374c392f078a81ad8
peidong <peidong@outlook.com>
2015-12-30 17:27:06 -0800

    small change

--- Edison/main/include/beaglebone_drone_include.h
+++ Edison/main/include/beaglebone_drone_include.h
@@ -26,7 +26,7 @@
 // #define PRINT_DEBUG_PWM
 // #define PRINT_DEBUG_THREAD
 // #define PRINT_CAR_MANUAL
-#define CALIBRATE_GYROSCOPE
+// #define CALIBRATE_GYROSCOPE
 //#define TIMER
 //#define TIMER_YAW_PITCH_ROLL
 //#define TIMER_PID
@@ -719,7 +719,7 @@ int update_T_drone_arrd_yaw_pitch_roll(struct T_drone *pT_drone){
         //int result_agm[9] = { arawx, arawy, arawz, grawx, grawy, grawz, mrawx, mrawy, mrawz };
 
         // printf("%6d,%6d,%6d\n",arawx, arawy, arawz);
-        printf("%6d,%6d,%6d\n",grawx, grawy, grawz);
+        // printf("%6d,%6d,%6d\n",grawx, grawy, grawz);
         ax = (float)arawx*aRes;
         ay = (float)arawy*aRes;
         az = (float)arawz*aRes;
@@ -752,7 +752,7 @@ int update_T_drone_arrd_yaw_pitch_roll(struct T_drone *pT_drone){
         pT_drone->arrd_yaw_pitch_roll[2] = roll;
 #ifdef PRINT_DEBUG_YAW_PITCH_ROLL
         if (pT_drone->nflag_enable_pwm_pid_ultrasound != 1){
-            // printf("yaw = %.1f\tpitch = %.1f\troll = %.1f\n",yaw, pitch, roll);
+            printf("yaw = %.1f\tpitch = %.1f\troll = %.1f\n",yaw, pitch, roll);
          //printf("yaw = %d\tpitch = %d\troll = %d\n",(int)yaw, (int)pitch, (int)roll);
         }
 #endif
